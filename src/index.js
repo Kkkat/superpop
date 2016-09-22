962183f206334a1caa1d384a80e0e2d296f2d517
xuekaima <422580963@qq.com>
2016-09-22 21:19:52 +0800

    add animation

--- src/index.js
+++ src/index.js
@@ -1,35 +1,31 @@
+var canvas, ctx;
+
 window.onload = function() {
-	var canvas = document.getElementById("ball");
+	canvas = document.getElementById("ball");
 	if(canvas.getContext) {
-		var ctx = canvas.getContext("2d");
-		var ball = new Ball(ctx);
+		ctx = canvas.getContext("2d");
+		var ball = new Ball();
 	}
 	DragDrop.enable();
 
-	// document.addEventListener("touchstart", function(e) {
-	// 	console.log(e);
-	// })
 }
 
 var DragDrop = function(){
 	var	dragging = null,
-		diffX = 0, 
+		diffX = 0,
 		diffY = 0;
 
 	function handleEvent(e) {
-		
+
 		// 获取事件和对象
 		var event = e ? e: window.event;
 		var target = e.target || e.srcElement;
-		
+
 		// 确定事件类型
 		switch(event.type) {
 			case "touchstart":
 				if(target.className.indexOf("draggable") > 1) {
 					dragging = target;
-					diffX = event.touches[0].clientX - target.parentNode.offsetLeft;
-					diffY = event.touches[0].clientY - target.parentNode.offsetTop;
-					// console.log(event.touches[0].screenX);
 				}
 				break;
 
@@ -37,6 +33,12 @@ var DragDrop = function(){
 				if(dragging !== null) {
 
 					// 指定位置
+					diffX = event.touches[0].clientX - target.parentNode.offsetLeft;
+					diffY = event.touches[0].clientY - target.parentNode.offsetTop;
+					if(diffX >= 115) diffX = 115;
+					if(diffX <= -15) diffX = -15;
+					if(diffY >= 115) diffY = 115;
+					if(diffY <= -15) diffY = -15;
 					dragging.style.left =  diffX + 'px';
 					dragging.style.top = diffY + 'px';
 
@@ -44,6 +46,8 @@ var DragDrop = function(){
 				break;
 
 			case "touchend":
+				dragging.style.left = "50%";
+				dragging.style.top = "50%";
 				dragging = null;
 				break;
 		}
@@ -63,7 +67,16 @@ var DragDrop = function(){
 		}
 	}
 
-}()
+}();
+
+ // requestAnim
+window.requestAnimFrame = (function(){
+    return  window.requestAnimationFrame       ||
+          	window.webkitRequestAnimationFrame ||
+          	window.mozRequestAnimationFrame    ||
+          	window.oRequestAnimationFrame      ||
+          	window.msRequestAnimationFrame
+})();
 
 // 球球
 function Ball() {
@@ -72,11 +85,20 @@ function Ball() {
 
 Ball.prototype = {
 
-	init: function(ctx) {
-		this.drawABall(ctx, 200, 30, 10, "#fff");
+	x: 200,
+
+	y: 100,
+
+	speedX: 10,
+
+	speedY: 20,
+
+	init: function() {
+		// this.drawABall(ctx, 200, 100, 10, "#ff5656");
+		this.runningBall();
 	},
 
-	drawABall: function(ctx, x, y, r, bColor) {
+	drawABall: function(x, y, r, bColor) {
 		ctx.save();
 		ctx.fillStyle = bColor;
 		ctx.beginPath();
@@ -84,5 +106,18 @@ Ball.prototype = {
 		ctx.closePath();
 		ctx.stroke();
 		ctx.fill();
+	},
+
+	runningBall: function() {
+		// console.log(this.runningBall);
+		var _this = this;
+		window.requestAnimationFrame(_this.runningBall);
+		// setTimeout(function() {
+		// 	_this.runningBall();
+		// }, 100);
+		// ctx.clearRect(0, 0, 1024, 640);
+		this.drawABall(this.x, this.y, 10, "#ff5656");
+		this.x += this.speedX;
+		this.y += this.speedY;
 	}
 }
